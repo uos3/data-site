efa03c133a3e8e1a23d6bbd33f69d9c0cbff9eee
corvidism <frankova@corvidism.com>
2016-04-28 23:12:59 +0100

    figuring out db schema and laravel migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+			$table->string('ip_address',45)->unique();
+			$table->boolean('blocked')->default(false);
+			$table->timestamps();
         });
     }
 
