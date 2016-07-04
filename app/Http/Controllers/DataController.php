3140b2fa70c03b967eefd45e11665a0e32a48b09
corvidism <frankova@corvidism.com>
2016-07-04 10:38:06 +0200

    data submit user access + matching unit tests + new TODO file

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Submission;
+use App\User;
 use App\Http\Requests;
+//use Request;
 
 class DataController extends Controller
 {
@@ -14,8 +16,84 @@ class DataController extends Controller
 		
     //
     
-    public function submit() {
-    	/*
+    public function submit(Request $request) {
+		
+		//if submission's api key isn't correct, abort.
+		
+		//error_log("says appkey is this: ".$request->app_key);
+		//error_log("env appkey is this: ".env('APP_KEY'));
+		
+		if (!isset($request->app_key)) {
+			abort(401, 'No app key supplied.'); //authenticate
+		} 
+		
+		if (env('APP_KEY') != $request->app_key) {
+			abort(401, 'Supplied app key "'.$request->app_key.'" is incorrect.'); //REauthenticate
+		}
+		
+		//get submitter's ip
+		$ip = $request->ip();
+		
+		
+		//$user = User::where('ip_address',$ip)->first();
+		$user = User::firstOrCreate(['ip_address'=>$ip]); //I think this might stop working once I have more than just ip. Lets figure that out later.
+		
+		//check if user isn't blocked		
+		if ($user->blocked) {
+			abort(403); //lolnope.
+		}
+		
+		//gather data
+		$data = [];
+		
+		/**
+		$data['cubesat_time'] = $request->cubesat_time; //check if it's... time-y?
+		$data['user_id'] = $user->id;
+		
+		//cubesat data. will change. TODO sanity/validity checks for everything.
+		$data['temperature'] = $request->temperature;  
+		$data['gyro_x'] = $request->gyro_x;
+		$data['gyro_y'] = $request->gyro_y;
+		$data['gyro_z'] = $request->gyro_z;
+		$data['accelerometer_x'] = $request->accelerometer_x;
+		$data['accelerometer_y'] = $request->accelerometer_y;
+		$data['accelerometer_z'] = $request->accelerometer_z;
+		
+		 */
+		//$submission = new Submission($data);
+		//$submission->save();
+		
+		return "Success.";
+		
+		
+		
+		/**
+		 * get ip
+check if ip in user db
+no: create user, get id
+yes: check if user banned
+	yes: abort
+	no: get user id
+		 */
+
+
+		//check if ip not in banlist
+		
+		//check if ip in user table
+		
+		//yes: get user id
+		
+		//no: create new user AND get id
+		
+		//get data
+		
+		//check if valid (numbers, floats, etc.)
+		
+		//yes: put into db
+		
+		//no: abort
+		
+		/*
 		 * app sends POST data through specific url
 		 * check if the APP KEY is correct (app key is embedded in the Java app)
 		 * 
@@ -29,8 +107,6 @@ class DataController extends Controller
 		 * 
 		 * 
 		 */ 
-		 
-		 return('yes');
     }
 	
 	/**
--- tests/DataControllerTest.php
+++ tests/DataControllerTest.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -7,19 +7,56 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class DataControllerTest extends TestCase
 {
     /**
-     * A basic functional test example.
-     *
-     * @return void
+     *	If the request has no app key, abort with 401.
+     *	@return void
      */
-    public function testSubmit()
-    {
-    	$this->post('data/submit')
-			->assertResponseOk();
-			
-		//give it a mocked up data
+    public function testSubmitNoAppKey() {
+		$response = $this->call('POST','data/submit',['say'=>'what']);
+		//curl -v -s -i -d "say=what" "http://cubehome_local/data/submit" 1> /dev/null
 		
-		//test if it ended up in the database
-    }
+		$this->assertResponseStatus('401'); 
+		$this->see("No app key supplied."); 
+	}
+	
+	/**
+     *	If the request has bad app key, abort with 401.
+     *	@return void
+     */
+	public function testSubmitBadAppKey() {
+		$bad_key = "swordfish";
+		$response = $this->call('POST','data/submit',['app_key'=>$bad_key]);
+		//curl -v -s -i -d "app_key=swordfish" "http://cubehome_local/data/submit" 1> /dev/null
+		
+		$this->assertResponseStatus('401'); 
+		$this->see('Supplied app key "'.$bad_key.'" is incorrect.'); 
+	}
+	
+	/**
+     *	If the request is comming from blocked IP, abort with 403.
+	 * 	@todo needs blocking/unblocking a user
+     *	@return void
+     */
+	public function testSubmitBlockedUser() {
+			
+		//get user by IP (127.0.0.1), block them
+		
+		$response = $this->call('POST','data/submit',['app_key'=>'OTe7K69Plj0nAFgX91sAszl2txy9TobF','cubesat_time'=>Carbon\Carbon::now(),'uploaded_at'=>Carbon\Carbon::now()]);
+		//curl -v -s -i -d "app_key=OTe7K69Plj0nAFgX91sAszl2txy9TobF" "http://cubehome_local/data/submit" 1> /dev/null
+		
+		//unblock user!
+	}
+	
+	/**
+     *	If everything is OK, return 200.
+     *	@return void
+     */
+	public function testSubmitOK() {
+		$response = $this->call('POST','data/submit',['app_key'=>'OTe7K69Plj0nAFgX91sAszl2txy9TobF','user_id'=>1,'cubesat_time'=>Carbon\Carbon::now(),'uploaded_at'=>Carbon\Carbon::now()]);
+		//curl -v -s -i -d "app_key=OTe7K69Plj0nAFgX91sAszl2txy9TobF" "http://cubehome_local/data/submit" 1> /dev/null
+		
+		$this->assertEquals('200',$response->status());
+		$this->see("Success.");
+	}
 	
 	public function testRedirect() {
 		$this->get('data/submit')
