91c9e929198b47f51a88b0e44c5f8b27bab31844
corvidism <frankova@corvidism.com>
2017-09-29 15:12:28 +0200

    (very) basic profile page setup

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -13,30 +13,26 @@
 	</div>	
 	<div class="row"> 
 		<div id="user-data" class="col-md-9 col-sm-12">
-			<dl class="submit-key">
-				<dt>Submit key:</dt>
-				<dd>{{$user->submit_key}}</dd>
-			</dl>
-			<dl>
-					<dt>Name:</dt>
-					<dd>
-						{{$user->name}}&nbsp;				  
-					</dd>
-					<dt>Affiliation (e.g. school, club, etc.):</dt>
-					<dd>
-						{{$user->affiliation}}&nbsp;
-					</dd>
-					<dt>Leaderboard position:</dt>
-					<dd>
-						@if ($user->public)
-		    				{{$leaderboard_position}}&nbsp;	
-		    			@else
-		    				(set profile to 'public' to show up on the leaderboard)
-		    			@endif
-						
-					</dd>
-			</dl>
-			<a href="">edit profile</a>
+			<div id="submit-key-block">
+				<h3>Submit key</h3>
+				<p>This is your submit key. You will need to paste this into the xxx plugin to submit data as this account.</p>
+				<p id="submit-key">{{$user->submit_key}}</p>				
+			</div>
+			
+			<form id="user-data" method="POST">
+				<div class="form-group">
+					<label for="input-name">Name:</label>
+					<input type="text" class="form-control" id="input-name" name="name">
+				</div>
+				<div class="form-group">
+					<label for="input-affiliation">Affiliation (e.g. school, club, etc.):</label>
+					<input type="text" class="form-control"  id="input-affiliation" name="affiliation">
+				</div>
+				<div class="form-group">
+					<label>Show me on the leaderboard: <input type="checkbox" id="input-public" name="public"></label>	
+				</div>
+				<input type="submit" class="btn btn-default" value="Save changes">
+			</form>
 		</div>			
 	</div>
 	    		        		
