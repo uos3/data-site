0ba923f490c8f6b4713184f8c0ce7e7c8af7ea2d
corvidism <frankova@corvidism.com>
2018-02-16 23:22:14 +0100

    WIP: JSON API, stubbing the parser.

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -6,10 +6,17 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use Carbon\Carbon;
 use Validator;
+use Log;
 
 /*my Eloquent models*/
 use App\Submission;
+use App\SubmissionBinary;
 use App\User;
+use App\SatConfig;
+use App\SatGPS;
+use App\SatHealth;
+use App\SatIMG;
+use App\SatStatus;
 use App\BlacklistedIP;
 
 class DataController extends Controller
@@ -58,15 +65,47 @@ class DataController extends Controller
     private function validateUser($user) {
       if ($user->blocked) {
         abort(403, 'Access denied.'); //lolnope.
-      }
+      };
 
       return TRUE;
-    };
+    }
+
+    /**
+    * STUB. Convert binary data to php array/object.
+    *
+    * @param string $data_encoded Base64-encoded binary data from the SDR plugin.
+    * @return Array $data Array of decoded values.
+    *
+    */
+    private function parseBinary($data_encoded) {
+      //feed the hash to the C++ parser
+      //
+
+      //STUB
+      $data = [
+        'hash'=>'',
+        'checksum'=>'',
+      ];
+
+      //use a Validator to validate all the values. Maybe several validator for the data parts, build them elsewhere, and reuse them???
+      $validator = Validator::make($data,[
+        'hash'=>'required'
+      ]);
+
+      if ($validator->fails()) {
+        return "things went bad";
+      }
+
+      //maybe instead: validate the fields inside the C++ parser, so I don't have to bother here?
+      //...why am I even validating? if the hash and CRC matches, the values MUST be correct, right?
+
+      return $data;
+    }
 
     public function submit(Request $request) {
+      //
+      //JSON input
 
-      //TODO log submission as string -- special log?
-      //Storage::append('file.log', 'Appended Text'); -- https://laravel.com/docs/5.2/filesystem#storing-files
 
     	//if submission's api key isn't correct, abort.
 
@@ -75,18 +114,21 @@ class DataController extends Controller
 
 
 
-    $this->validateAccess($request);
+    //$this->validateAccess($request);
 
-    $user = $this->getUserIfExists();
+    //$user = $this->getUserIfExists();
 
-    if ($user) {
+    /*if ($user) {
         $this->validateUser($user);
-    }
+    }*/
+    //Log::info("TEST");
+    $app_key = $request->input("app_key");
+    $submit_key = $request->input("submit_key");
 
+    $data_encoded = $request->input("data");
+    $data = $this->parseBinary($data_encoded);
 
-    $data = $request->all();  //possibly only part of data? -- $request->only([a,b,c]), $request->except([a,b,c])
-
-    $this->checksum($data); //TODO CHECKSUM -- throw error???
+    return "Fake Success. App key: ".$app_key.", submit key: ".$submit_key;
 
     //TODO pick which type of upload this is (one-character type number? -- set in .env, no magic numbers!)
 
@@ -94,7 +136,7 @@ class DataController extends Controller
 
 
 
-    
+
 		//add computed values
 		$data['uploaded_at'] = Carbon::now()->toDateTimeString();
 		$data['ip_address'] = $ip;
